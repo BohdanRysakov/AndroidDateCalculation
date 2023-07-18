cec06e6db663a0ab011ca65423674cebca0383e0
Bohdan Rysakov <bogdanrysokov@gmail.com>
2023-07-18 18:36:53 +0300

    added readme

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Project for training Android developing
+_________
+As a Java Developer i didn't used to Kotlin & Android, so this is a project to improve myself 
+on new field 
+
+## Main info i took from Udemy course - **The Complete Android 12 & Kotlin Development Masterclass** ##
+
+#### Main purpose of this project:
+* Learn Kotlin syntax
+* Try myself in Android Developing
+
+*This is simple project with not complicated logic*
+
+As a result of my practice in Android developing i'm planning to create music player 
+
+--------------------
+![First img](/images/initialPhoto.png)
+![Second img](/images/selectDate.png)
+![Third img](/images/result.png)
--- app/src/main/java/com/example/datecalculation/MainActivity.kt
+++ app/src/main/java/com/example/datecalculation/MainActivity.kt
@@ -1,165 +1,89 @@
 package com.example.datecalculation
 
-import android.annotation.SuppressLint
 import android.app.DatePickerDialog
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvSelectedDateFrom : TextView?  = null
-    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-    @RequiresApi(Build.VERSION_CODES.O)
-    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-    @RequiresApi(Build.VERSION_CODES.O)
-    val current = LocalDateTime.now().format(formatter)
-    @RequiresApi(Build.VERSION_CODES.O)
-    private var dateFrom : Date? = sdf.parse(current)
-    @RequiresApi(Build.VERSION_CODES.O)
-    private var dateTo : Date?  = sdf.parse(current)
-    private var tvDiffSec:TextView? =  null
-    private var tvDiffMin:TextView? =  null
-    private var tvDiffHour:TextView? =  null
-    private var tvDiffDays:TextView? =  null
-    private var tvDiffWeeks:TextView? =  null
-    private var tvDiffMonth:TextView? = null
-    private var tvDiffYear:TextView? =  null
+    // Объявляем TextView для отображения выбранных дат и TextViews для отображения разницы во времени.
+    private lateinit var tvSelectedDateFrom: TextView
+    private val dateValuesTextViews: Array<TextView> by lazy {
+        arrayOf(findViewById(R.id.secDiff), findViewById(R.id.minDiff), findViewById(R.id.hourDiff),
+            findViewById(R.id.dayDiff), findViewById(R.id.weeksDiff), findViewById(R.id.mountsDiff),
+            findViewById(R.id.yearsDiff))
+    }
+    // SimpleDateFormat для преобразования даты в строку и обратно.
+    private val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    // Инициализируем даты как текущую дату.
+    private var dateFrom = Date()
+    private var dateTo = Date()
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Находим TextViews и задаем обработчики событий клика для кнопок.
         tvSelectedDateFrom = findViewById(R.id.tvSelectedDates)
-         tvDiffSec =  findViewById(R.id.secDiff)
-          tvDiffMin =  findViewById(R.id.minDiff)
-         tvDiffHour =  findViewById(R.id.hourDiff)
-          tvDiffDays =  findViewById(R.id.dayDiff)
-         tvDiffWeeks =  findViewById(R.id.weeksDiff)
-          tvDiffMonth =  findViewById(R.id.mountsDiff)
-          tvDiffYear =  findViewById(R.id.yearsDiff)
-
-        val btnDateFrom : Button = findViewById(R.id.btnDatePickerFrom)
-        val btnDateTo : Button = findViewById(R.id.btnDatePickerTo)
-
-
-        btnDateFrom.setOnClickListener {
-
-            clickDatePickFrom()
-
-        }
-        btnDateTo.setOnClickListener {
-            clickDatePickTo()
-
-        }
+        findViewById<Button>(R.id.btnDatePickerFrom).setOnClickListener { clickDatePicker(::setDateFrom) }
+        findViewById<Button>(R.id.btnDatePickerTo).setOnClickListener { clickDatePicker(::setDateTo) }
     }
 
-    @SuppressLint("SetTextI18n")
-
-    private fun changeDifference() {
-        var diff = Math.abs((dateTo?.time ?: 0) - (dateFrom?.time ?:0 ))
-        tvDiffSec?.text = "${diff/1000.0} sec"
-        tvDiffMin?.text = "${diff/60000.0} mins"
-        val hours = diff/3600000.0
-        println(hours)
-        tvDiffHour?.text = "$hours hours"
-        val days = hours/24.0
-        println(days)
-        tvDiffDays?.text = "$days days"
-        val weeks = days/7.0
-        println(weeks)
-        tvDiffWeeks?.text = "$weeks weeks"
-        val month = weeks/4.34
-        println(month)
-        tvDiffMonth?.text = "$month months"
-        val years = days/365.0
-        println(years)
-        tvDiffYear?.text = "$years years"
-    }
-
-    @SuppressLint("SetTextI18n")
-    fun clickDatePickFrom() {
+    // Функция для отображения DatePickerDialog и установки выбранной даты.
+    private fun clickDatePicker(dateSetter: (Date) -> Unit) {
         val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)+1
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
         DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{ view, selectedYear, selectedMonth, selectedDayOfMonth ->
-
-
-                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
-                dateFrom = sdf.parse(selectedDate)
-                if(tvSelectedDateFrom != null){
-                    if((tvSelectedDateFrom?.text?.length)== 0) {
-
-                        dateTo = sdf.parse("${day}/${month}/${year}")
-                        val tmpNOTICEME = dateTo.toString()
-
-                        tvSelectedDateFrom?.text = "$selectedDate - ${day}/${month}/${year}"
-                    }
-                    else {
-                        tvSelectedDateFrom!!.text =
-                            selectedDate + tvSelectedDateFrom!!.text.toString()
-                    }
-                }
-                changeDifference();
+            DatePickerDialog.OnDateSetListener { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                // Парсим выбранную дату.
+                val selectedDate = sdf.parse("$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear")
+                selectedDate?.let {
+                    // Устанавливаем выбранную дату.
+                    dateSetter(it)
+                    // Обновляем текст с выбранными датами.
+                    updateDatesText()
+                    // Вычисляем разницу между датами.
+                    changeDifference()
+                } ?: Toast.makeText(this, "Error parsing date", Toast.LENGTH_SHORT).show()
             },
-            year,
-            month,
-            day
-
-            ).show()
-        println("$year ; $month ; $day")
-    }
-    @RequiresApi(Build.VERSION_CODES.O)
-    @SuppressLint("SetTextI18n")
-    fun clickDatePickTo() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{ view, selectedYear, selectedMonth, selectedDayOfMonth ->
-
-                val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
-                dateTo = sdf.parse(selectedDate)
-                if(tvSelectedDateFrom != null){
-                    if((tvSelectedDateFrom?.text?.length)== 0) {
-                        tvSelectedDateFrom?.text = " - $selectedDate"
-                    }
-                    else {
-                        tvSelectedDateFrom!!.text =
-                            tvSelectedDateFrom!!.text.toString() +" - $selectedDate"
-
-                    }
-
-                }
-                changeDifference()
-                test()
-
-
-            },
-            year,
-            month,
-            day
-
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
         ).show()
-        println("$year ; $month ; $day")
-
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun test() {
-        tvDiffYear?.text = "dsdsd years"
+    // Устанавливаем dateFrom.
+    private fun setDateFrom(date: Date) {
+        dateFrom = date
+    }
+
+    // Устанавливаем dateTo.
+    private fun setDateTo(date: Date) {
+        dateTo = date
+    }
+
+    // Обновляем текст в TextView с выбранными датами.
+    private fun updateDatesText() {
+        tvSelectedDateFrom.text = "${sdf.format(dateFrom)} - ${sdf.format(dateTo)}"
+    }
+
+    // Вычисляем и отображаем разницу между датами в различных единицах времени.
+    private fun changeDifference() {
+        val diff = Math.abs(dateTo.time - dateFrom.time)
+        val timeUnits = arrayOf(1000, 60000, 3600000, 86400000, 604800000, 2629800000, 31557600000)
+        timeUnits.forEachIndexed { index, unit ->
+            dateValuesTextViews[index].text = "${diff / unit.toDouble()}"
+        }
     }
 }
+
+
+
+
+
+
